31b6f63fc65611719523c3426981da35b16d897a
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-02-29 23:13:35 +0100

    fix FAQ

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -1,93 +1,20 @@
+import Fqadw from "../Components/Faq/Fqadw";
+
 function FAQ() {
   return (
     <div
       style={{ direction: "ltr" }}
-      className="w-[1300px]  mx-auto py-32  relative  max-md:w-screen"
+      className="w-[1300px]  mx-auto   relative  max-md:w-screen"
     >
       <div className="text-5xl font-bold text-center py-10">
         {" "}
         الاسئلة الاكثر شيوعا{" "}
       </div>
-      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
-        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
-          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
-          كيف اقوم باعادة استعادة تدوير المودام ؟
-        </div>
-        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
-          <div className="text-red-500 text-center font-bold text-2xl ">
-            {" "}
-            الجواب{" "}
-          </div>
-          لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
-          من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
-          للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
-          الكهرباء بمأخذ الطاقة لإعادة تشغيل المودم. انتظر حتى يبدأ المودم في
-          التشغيل بشكل كامل، قد تستغرق هذه العملية بضع دقائق. بمجرد أن يكون
-          المودم مشغّلًا بالكامل ويعمل بشكل صحيح، يمكنك محاولة الاتصال بالإنترنت
-          للتأكد من أن الإعادة تمت بنجاح. للتأكد من أن الإعادة تمت بنجاح. للتأكد
-          من أن الإعادة تمت بنجاح.
-        </div>
-      </div>
-      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
-        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
-          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
-          كيف اقوم باعادة استعادة تدوير المودام ؟
-        </div>
-        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
-          <div className="text-red-500 text-center font-bold text-2xl ">
-            {" "}
-            الجواب{" "}
-          </div>
-          لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
-          من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
-          للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
-          الكهرباء بمأخذ الطاقة لإعادة تشغيل المودم. انتظر حتى يبدأ المودم في
-          التشغيل بشكل كامل، قد تستغرق هذه العملية بضع دقائق. بمجرد أن يكون
-          المودم مشغّلًا بالكامل ويعمل بشكل صحيح، يمكنك محاولة الاتصال بالإنترنت
-          للتأكد من أن الإعادة تمت بنجاح. للتأكد من أن الإعادة تمت بنجاح. للتأكد
-          من أن الإعادة تمت بنجاح.
-        </div>
-      </div>
-      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
-        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
-          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
-          كيف اقوم باعادة استعادة تدوير المودام ؟
-        </div>
-        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
-          <div className="text-red-500 text-center font-bold text-2xl ">
-            {" "}
-            الجواب{" "}
-          </div>
-          لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
-          من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
-          للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
-          الكهرباء بمأخذ الطاقة لإعادة تشغيل المودم. انتظر حتى يبدأ المودم في
-          التشغيل بشكل كامل، قد تستغرق هذه العملية بضع دقائق. بمجرد أن يكون
-          المودم مشغّلًا بالكامل ويعمل بشكل صحيح، يمكنك محاولة الاتصال بالإنترنت
-          للتأكد من أن الإعادة تمت بنجاح. للتأكد من أن الإعادة تمت بنجاح. للتأكد
-          من أن الإعادة تمت بنجاح.
-        </div>
-      </div>
-      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
-        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
-          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
-          كيف اقوم باعادة استعادة تدوير المودام ؟
-        </div>
-        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
-          <div className="text-red-500 text-center font-bold text-2xl ">
-            {" "}
-            الجواب{" "}
-          </div>
-          لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
-          من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
-          للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
-          الكهرباء بمأخذ الطاقة لإعادة تشغيل المودم. انتظر حتى يبدأ المودم في
-          التشغيل بشكل كامل، قد تستغرق هذه العملية بضع دقائق. بمجرد أن يكون
-          المودم مشغّلًا بالكامل ويعمل بشكل صحيح، يمكنك محاولة الاتصال بالإنترنت
-          للتأكد من أن الإعادة تمت بنجاح. للتأكد من أن الإعادة تمت بنجاح. للتأكد
-          من أن الإعادة تمت بنجاح.
-        </div>
-      </div>
+      <Fqadw />
+      <Fqadw />
+      <Fqadw />
+      <Fqadw />
+      <Fqadw />
     </div>
   );
 }
