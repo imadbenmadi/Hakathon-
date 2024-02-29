f6a941e92930e199c568da00132c8c4a184273b1
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 16:40:14 +0100

    .

--- src/Components/Prodact/Prodect.jsx
+++ src/Components/Prodact/Prodect.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import prodact from "../../assets/images/download.jpg";
 function Prodect() {
   return (
-    <div className="mx-auto my-5 relative max-md:flex-col  flex gap-10 border border-grey-100 mx-5 md:px-5 py-10 rounded-lg bg-slate-200">
+    <div className="mx-auto my-5 relative max-md:flex-col  flex gap-10 border border-grey-100 mx-5 md:px-5 py-10 rounded-lg bg-slate-100 border-gray_white shadow-sm">
       <div className=" w-[48%] max-md:w-fit px-2 max-md:order-2 ">
         <div className="text-2xl  "> كيفية تشغيل مودم اتصالات الجزائر </div>
         <div className="text-xl">
