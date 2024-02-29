0dad5975c6696ecc2cefedd751c38c5a303aeaa8
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-02-29 14:48:25 +0100

    salah

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router";
 import NavBar from "./Components/NavBar";
 function App() {
-  return <div className="">
-    <NavBar />
-    <Outlet />
-  </div>;
+  return (
+    <div className="">
+      {/* <NavBar /> */}
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Faq/FaqLine.jsx
+++ src/Components/Faq/FaqLine.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import rowaBlue from "../../assets/images/akar-icons_chevron-right-blue.svg";
-import rowa from "../../assets/images/akar-icons_chevron-right.svg";
-function FaqLine({ text, color, status }) {
+import React from 'react'
+
+function 
+() {
   return (
-    <div className="w-full h-16 px-4 py-6 bg-white shadow flex-col justify-start items-start inline-flex">
-      <div className="justify-center items-center gap-6 inline-flex">
-        <div className="justify-start items-start gap-4 flex">
-          <div className={`w-6 h-6 ${color}  rounded-full`} />
-          <div className="w-96 text-red text-base font-normal font-['Inter'] leading-normal">
-            {text}{" "}
-          </div>
-        </div>
-        <div className="w-6 h-6 relative">
-          {status ? <img src={rowaBlue} alt="" /> : <img src={rowa} alt="" />}
-        </div>
-      </div>
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default FaqLine;
+export default 
