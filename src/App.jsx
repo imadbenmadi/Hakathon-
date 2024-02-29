4ef2237b1feeab0633ffa88ccace1d14dbfcd761
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 13:54:42 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router";
+import NavBar from "./Components/NavBar";
 function App() {
   return <div className=" text-red-500">
     hello 
+    <NavBar/>
     <Outlet/>
     </div>;
 }
