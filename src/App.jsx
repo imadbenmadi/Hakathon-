1b16d453c9e015bfc2885f2fc15a30f551a57c58
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 14:56:29 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import NavBar from "./Components/NavBar";
 function App() {
-  return (
-    <div className="">
-      <NavBar />
-      <Outlet />
-    </div>
-  );
+    return (
+        <div className="cairo-uniquifier ">
+            <NavBar />
+            <div className=" mt-28">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default App;
