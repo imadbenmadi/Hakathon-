d2f82ee9696a3355d0c7da3e3385a697da0efc29
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 22:24:40 +0100

    .

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,16 +8,16 @@ function NavBar() {
     const [showMenu, setShowMenu] = useState(false);
     const [isSmall, setIsSmall] = useState(false);
     useEffect(() => {
-        const handelResize = () => {
+        const handleResize = () => {
             setIsSmall(window.innerWidth <= 768);
         };
-        window.addEventListener("resize", handelResize);
-        handelResize();
+        window.addEventListener("resize", handleResize);
+        handleResize();
         return () => {
-            window.removeEventListener("resize", handelResize);
+            window.removeEventListener("resize", handleResize);
         };
     }, []);
-    console.log(show)
+    console.log(show);
     const navigate = useNavigate();
     const Links = [
         {
@@ -37,17 +37,43 @@ function NavBar() {
             path: "/NewProduct",
         },
     ];
-    return isSmall ? (
-        <div className="flex justify-between items-center p-3">
-            <img src={Logo} alt="" className="w-28" />
-            <CiMenuBurger
-                size={50}
-                className="cursor-pointer ml-auto"
-                onClick={() => setShowMenu(!showMenu)}
-            />
-            {showMenu && (
-                <div>
-                    <div className="fixed flex justify-center items-center flex-col gap-10 shadow-md top-20 left-0 bg-white w-full h-[30%] z-50">
+    return (
+        <>
+            {isSmall ? (
+                <div className="flex justify-between items-center p-3">
+                    <img src={Logo} alt="" className="w-28" />
+                    <CiMenuBurger
+                        size={50}
+                        className="cursor-pointer ml-auto"
+                        onClick={() => setShowMenu(!showMenu)}
+                    />
+                    {showMenu && (
+                        <div>
+                            <div className="fixed flex justify-center items-center flex-col gap-10 shadow-md top-20 left-0 bg-white w-full h-auto p-5 z-50">
+                                {Links.map((link, index) => (
+                                    <Link
+                                        key={index}
+                                        to={link.path}
+                                        className="text-xl text-gray-600 hover:text-blue-500 transition-all duration-300"
+                                    >
+                                        {link.name}
+                                    </Link>
+                                ))}
+                            </div>
+                            <div
+                                className="w-screen h-screen absolute top-0 left-0 z-30"
+                                onClick={() => setShowMenu(false)}
+                            ></div>
+                        </div>
+                    )}
+                </div>
+            ) : (
+                <div className="w-full bg-white fixed flex justify-between items-center top-0 left-0 z-30 p-5 shadow-lg">
+                    <div className="flex justify-start items-center gap-3">
+                        <img src={Logo} alt="" className="w-28" />
+                        <p></p>
+                    </div>
+                    <div className="flex justify-center items-center gap-14">
                         {Links.map((link, index) => (
                             <Link
                                 key={index}
@@ -58,42 +84,17 @@ function NavBar() {
                             </Link>
                         ))}
                     </div>
-                    <div
-                        className="w-screen h-screen absolute top-0 left-0 z-30"
-                        onClick={() => setShowMenu(false)}
-                    ></div>
+                    <div>
+                        <button
+                            onClick={() => navigate("/Login")}
+                            className="w-32 p-3 bg-blue-500 text-white text-xl rounded-[8px] transition-all duration-300 hover:scale-105"
+                        >
+                            Login
+                        </button>
+                    </div>
                 </div>
             )}
-        </div>
-    ) : (
-        <div className="w-full h-[10%] bg-white fixed flex justify-between items-center top-0 left-0 z-30 p-5 shadow-lg">
-            {/* logo */}
-            <div className="flex justify-start items-center gap-3">
-                <img src={Logo} alt="" className="w-28" />
-                <p></p>
-            </div>
-            {/* links */}
-            <div className="flex justify-center items-center gap-14">
-                {Links.map((link, index) => (
-                    <Link
-                        key={index}
-                        to={`${link.path}`}
-                        className="text-xl text-gray-600 hover:text-blue-500 transition-all duration-300"
-                    >
-                        {link.name}
-                    </Link>
-                ))}
-            </div>
-            {/* auth */}
-            <div>
-                <button
-                    onClick={() => navigate("/Login")}
-                    className="w-32 p-3 bg-blue-500 text-white text-xl rounded-[8px] transition-all duration-300 hover:scale-105"
-                >
-                    Login
-                </button>
-            </div>
-        </div>
+        </>
     );
 }
 
