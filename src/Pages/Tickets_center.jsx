143709aaf3d927275ea42b6429f65d726499b353
benkedda louai <benkedda.louaisiradjeddine@univ-ouargla.dz>
2024-02-29 21:48:55 +0100

    ticket

--- src/Pages/Tickets_center.jsx
+++ src/Pages/Tickets_center.jsx
@@ -1,29 +1,19 @@
 import React from "react";
-
+import { useNavigate } from "react-router";
+import agency from './data'
 function Tickets_center() {
     const [clicked, setClicked] = React.useState(false);
-    console.log(clicked)
-
+    const navigate = useNavigate();
     return <div className="relative w-fit h-full mx-auto p-3 bg-black bg-opacity-5 flex flex-col justify-center items-center gap-5">
-        {clicked && (
-            <div className="absolute left-[110%] top-0 w-full h-full bg-black">louai</div>
-        )}
-        <div onClick={() => setClicked(!clicked)} className="flex cursor-pointer justify-center items-center gap-10 border-b border-solid border-black border-opacity-15 p-3">
-            <p className="text-[23px]">Ourgla agency</p>
-            <p className="text-[23px]">El nacer neighbor hood</p>
-        </div>
-        <div className="flex cursor-pointer justify-center items-center gap-10 border-b border-solid border-black border-opacity-15 p-3">
-            <p className="text-[23px]">Ourgla agency</p>
-            <p className="text-[23px]">El nacer neighbor hood</p>
-        </div>
-        <div className="flex cursor-pointer justify-center items-center gap-10 border-b border-solid border-black border-opacity-15 p-3">
-            <p className="text-[23px]">Ourgla agency</p>
-            <p className="text-[23px]">El nacer neighbor hood</p>
-        </div>
-        <div className="flex cursor-pointer justify-center items-center gap-10 border-b border-solid border-black border-opacity-15 p-3">
-            <p className="text-[23px]">Ourgla agency</p>
-            <p className="text-[23px]">El nacer neighbor hood</p>
-        </div>
+        {agency.map((item, index) => (
+            <div key={index} className="w-full h-20 bg-white bg-opacity-50 rounded-lg flex justify-between items-center p-3">
+                <div className="flex justify-start items-start flex-col">
+                    <h2>{item.name}</h2>
+                    <p>{item.address}</p>
+                </div>
+                <button onClick={navigate(`/Tickets/center/${item.id}`)} className="w-20 h-10 bg-blue-500 rounded-lg text-white">Select</button>
+            </div>
+        ))}
     </div>;
 }
 
---
+++ src/Pages/data.js
@@ -0,0 +1,24 @@
+const agency = [
+  {
+    id: 1,
+    name: "Ourgla agency",
+    address: "El nacer neighbor hood",
+  },
+  {
+    id: 2,
+    name: "Touggourt agency",
+    address: "Touggourt neighbor hood",
+  },
+  {
+    id: 3,
+    name: "Oran agency",
+    address: "Hammou Mokhtar neighbor hood",
+  },
+  {
+    id: 4,
+    name: "Hydra agency",
+    address: "El Bitoum neighbor hood",
+  },
+];
+
+export default agency;
