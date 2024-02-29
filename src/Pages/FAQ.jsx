31f90446caf7f1a54d178363ffaab9d821507b38
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-02-29 14:03:04 +0100

    salah

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import FaqLine from "../Components/Faq/FaqAnsewar";
+import FaqAnsewar from "../Components/Faq/FaqAnsewar";
 
 function FAQ() {
   return (
-    <div>FAQ</div>
-  )
+    <div className="w-screen flex  ">
+      <div className="w-[45%] relative bg-black  ">
+        <div className="  h-fit">
+          <FaqLine
+            text="عندي خلل في الشبكة "
+            color="bg-indigo-300"
+            status={false}
+          />
+          <FaqLine
+            text="عندي خلل في الشبكة "
+            color="bg-indigo-500"
+            status={true}
+          />
+          <FaqLine
+            text="عندي خلل في الشبكة "
+            color="bg-indigo-300"
+            status={false}
+          />
+          <FaqLine
+            text="عندي خلل في الشبكة "
+            color="bg-indigo-300"
+            status={false}
+          />
+          <FaqLine
+            text="عندي خلل في الشبكة "
+            color="bg-indigo-300"
+            status={false}
+          />
+        </div>
+      </div>
+      <div className="w-[45%] ">
+        <FaqAnsewar />
+      </div>
+    </div>
+  );
 }
 
-export default FAQ
+export default FAQ;
