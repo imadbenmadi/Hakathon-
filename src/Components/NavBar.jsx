3a698ef982d14e4c790a01d28fc5e080e28ac9f7
benkedda louai <benkedda.louaisiradjeddine@univ-ouargla.dz>
2024-02-29 14:03:18 +0100

    commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { Logo } from '../assets/images/download-removebg-preview.png'
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className='w-full h-20 bg-black fixed top-0 left-0 z-50'>
+      {/* logo */}
+      <div>
+        <img src="" alt="" />
+        <p>ATL</p>
+      </div>
+      {/* links */}
+      {/* auth */}
+    </div>
   )
 }
 
