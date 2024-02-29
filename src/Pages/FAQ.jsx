4ba1c638927a6dc7596308e1bd4f69589705bcec
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-02-29 15:22:41 +0100

    finch all asssement

--- src/Pages/FAQ.jsx
+++ src/Pages/FAQ.jsx
@@ -2,15 +2,22 @@ function FAQ() {
   return (
     <div
       style={{ direction: "ltr" }}
-      className="w-[1300px]  mx-auto py-10  relative"
+      className="w-[1300px]  mx-auto py-32  relative  max-md:w-screen"
     >
-      <div className=" flex py-5 ">
-        <div className="w-[45%]  relative text-2xl">
-          <div className="text-red-500"> السؤال </div>
+      <div className="text-5xl font-bold text-center py-10">
+        {" "}
+        الاسئلة الاكثر شيوعا{" "}
+      </div>
+      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
+        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
+          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
           كيف اقوم باعادة استعادة تدوير المودام ؟
         </div>
-        <div className="w-[45%] relative text-2xl ">
-          <div className="text-red-500  "> الجواب </div>
+        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
+          <div className="text-red-500 text-center font-bold text-2xl ">
+            {" "}
+            الجواب{" "}
+          </div>
           لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
           من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
           للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
@@ -21,13 +28,16 @@ function FAQ() {
           من أن الإعادة تمت بنجاح.
         </div>
       </div>
-      <div className=" flex py-5 ">
-        <div className="w-[45%]  relative text-2xl">
-          <div className="text-red-500"> السؤال </div>
+      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
+        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
+          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
           كيف اقوم باعادة استعادة تدوير المودام ؟
         </div>
-        <div className="w-[45%] relative text-2xl ">
-          <div className="text-red-500  "> الجواب </div>
+        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
+          <div className="text-red-500 text-center font-bold text-2xl ">
+            {" "}
+            الجواب{" "}
+          </div>
           لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
           من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
           للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
@@ -38,13 +48,16 @@ function FAQ() {
           من أن الإعادة تمت بنجاح.
         </div>
       </div>
-      <div className=" flex py-5 ">
-        <div className="w-[45%]  relative text-2xl">
-          <div className="text-red-500"> السؤال </div>
+      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
+        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
+          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
           كيف اقوم باعادة استعادة تدوير المودام ؟
         </div>
-        <div className="w-[45%] relative text-2xl ">
-          <div className="text-red-500  "> الجواب </div>
+        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
+          <div className="text-red-500 text-center font-bold text-2xl ">
+            {" "}
+            الجواب{" "}
+          </div>
           لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
           من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
           للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
@@ -55,13 +68,16 @@ function FAQ() {
           من أن الإعادة تمت بنجاح.
         </div>
       </div>
-      <div className=" flex py-5 ">
-        <div className="w-[45%]  relative text-2xl">
-          <div className="text-red-500"> السؤال </div>
+      <div className=" flex py-5 text-end gap-20 max-md:gap-2 max-md:flex-col  ">
+        <div className="w-[45%]  relative font-bold text-2xl  max-md:w-screen max-md:px-5 ">
+          <div className="text-red-500 font-bold text-2xl "> السؤال </div>
           كيف اقوم باعادة استعادة تدوير المودام ؟
         </div>
-        <div className="w-[45%] relative text-2xl ">
-          <div className="text-red-500  "> الجواب </div>
+        <div className="w-[45%] relative text-2xl  max-md:w-screen max-md:px-5  ">
+          <div className="text-red-500 text-center font-bold text-2xl ">
+            {" "}
+            الجواب{" "}
+          </div>
           لإعادة تدوير المودم، يمكن اتباع الخطوات التالية: قم بفصل سلك الكهرباء
           من مأخذ الطاقة الخاص بالمودم، ثم انتظر لمدة 30 ثانية إلى دقيقة واحدة
           للسماح بتفريغ أي طاقة متبقية في المودم. بعد ذلك، قم بإعادة توصيل سلك
