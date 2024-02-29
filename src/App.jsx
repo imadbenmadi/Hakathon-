570b8e8a367590b402005905150b51fdae2792b7
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 14:15:44 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet } from "react-router";
 import NavBar from "./Components/NavBar";
 function App() {
-  return <div className=" text-red-500">
-    hello 
-    <NavBar/>
-    <Outlet/>
-    </div>;
+    return (
+        <div className=" ">
+            hello
+            <NavBar />
+            <Outlet />
+        </div>
+    );
 }
 
 export default App;
