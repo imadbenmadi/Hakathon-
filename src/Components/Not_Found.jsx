91189d8dbf7c73aecf167106f06bdd071ad47e9c
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-29 16:34:34 +0100

    .

--- src/Components/Not_Found.jsx
+++ src/Components/Not_Found.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Not_Found() {
-  return (
-    <div>Not_Found</div>
-  )
+    return (
+        <div className="flex items-center justify-center text-gray font-bold pt-16">
+            <div className="p-8 bg-white rounded-md shadow-lg text-center">
+                <h1 className="text-3xl md:text-5xl mb-4 text-gray-800">
+                    عذرًا! هذه الصفحة غير موجودة
+                </h1>
+                <p className="text-lg mb-8 text-gray-700">
+                    يمكن أن يكون الصفحة التي تبحث عنها قيد الإنشاء أو تم نقلها.
+                </p>
+                <p className="text-sm text-gray-600">
+                    تحقق مرتين من عنوان URL أو ارجع إلى{" "}
+                    <Link to={"/"} className="text-green">
+                        الصفحة الرئيسية.
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
 }
 
-export default Not_Found
+export default Not_Found;
